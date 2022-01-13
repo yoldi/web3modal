99464e3aa83b7ae9e0534af6d4d596c4b9a5be6b
Vladislav Romanov <romanov.itmo@gmail.com>
2022-01-13 16:42:52 +0300

    add dist folder

---
+++ dist/providers/connectors/index.d.ts
@@ -0,0 +1,15 @@
+import noinjectedprovider from "./noinjectedprovider";
+import injected from "./injected";
+import walletconnect from "./walletconnect";
+import portis from "./portis";
+import fortmatic from "./fortmatic";
+import torus from "./torus";
+import arkane from "./arkane";
+import authereum from "./authereum";
+import burnerconnect from "./burnerconnect";
+import mewconnect from "./mewconnect";
+import dcentwallet from "./dcentwallet";
+import bitski from "./bitski";
+import frame from "./frame";
+export { noinjectedprovider, injected, walletconnect, portis, torus, fortmatic, arkane, authereum, burnerconnect, mewconnect, dcentwallet, bitski, frame };
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/providers/connectors/noinjectedprovider.d.ts
@@ -0,0 +1,8 @@
+declare const NoInjectedProvider: () => Promise<{
+    name: string;
+    returnTrue: boolean;
+    on: () => void;
+    off: () => void;
+}>;
+export default NoInjectedProvider;
+//# sourceMappingURL=noinjectedprovider.d.ts.map
